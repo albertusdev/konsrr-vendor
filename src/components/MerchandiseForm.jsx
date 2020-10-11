8307bbe11c86c124b41a74b80253481d4a4089e7
Raihan Ramadistra <ramadistra@gmail.com>
2020-10-11 20:27:05 +0700

    merchandise form

--- src/components/MerchandiseForm.jsx
+++ src/components/MerchandiseForm.jsx
@@ -5,7 +5,7 @@ import * as firebase from 'firebase';
 
 export default function MerchandiseForm() {
   const auth = useSelector(state => state.auth);
-  const { register, handleSubmit, errors } = useForm();
+  const { register, handleSubmit, errors } = useForm({defaultValues: {stock: 0, price:0}});
 
   const onSubmit = (data) => {
 
@@ -25,33 +25,34 @@ export default function MerchandiseForm() {
   };
 
   return <div>
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col">
-      <input placeholder="Merchandise Name" className="text-2xl outline-none mb-2" name="name" ref={register}></input>
+      <input placeholder="Merchandise Name" className="text-2xl outline-none mt-4 mb-2" name="name" ref={register}></input>
       </div>
 
-      <div className="flex flex-col">
+      <div className="flex flex-col mb-4">
         <label className="mb-1">Description</label>
         <textarea rows="3" className="px-1 outline-none border-gray-500 border-solid border-2" name="description" ref={register}></textarea>
       </div>
+      
+      <label className="mr-2 mb-1">Picture</label>
+      <div>
+        <input name="picture" ref={register} type="file"></input>
+      </div>
 
-      <div className="flex">
+      <div className="flex mt-4">
         <div>
-          <label>Stock</label>
+          <label className="mr-2">Stock</label>
           <input name="stock" ref={register}></input>
         </div>
-
         <div>
-          <label>Price</label>
+          <label className="mr-2">Price (IDR)</label>
           <input name="price" ref={register}></input>
         </div>
       </div>
 
-      <label>Picture</label>
-      <input name="picture" ref={register} type="file"></input>
-
-      <div>
-        <input type="submit" value="Add Merchandise"></input>
+      <div className="mt-6">
+        <input className="p-2" type="submit" value="Add Merchandise"></input>
       </div>
     </form>
   </div>
