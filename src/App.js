d081606584a88ab8d0814019b90158b9543af371
Shafiya Adzhani <adz.arsym@gmail.com>
2020-10-11 13:39:48 +0800

    move create concert

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import * as firebase from 'firebase';
 import { Router, Route, Switch } from "react-router";
 import Home from "./components/Home";
-import CreateConcert from "./components/CreateConcert";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import 'firebase/auth';
@@ -51,8 +50,8 @@ function App() {
     return unsubscribe
   }, [dispatch]);
 
-  console.log("HOHO", dispatch)
-  return auth.isLoading ? <div className="text-white">loading...<CreateConcert /></div> : (
+  console.log("HOHO", auth)
+  return auth.isLoading ? <div className="text-white">loading...</div> : (
     <div className="max-w-md mx-auto flex p-6 bg-primary-1 mt-10 rounded-lg shadow-xl">
       <div className="ml-6 pt-1">
         <Router history={history}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import * as firebase from 'firebase';
 import 'firebase/auth';
+import CreateConcert from "./CreateConcert";
 
 export default function Home() {
   return <div>
     <button className="text-white" onClick={() => firebase.auth().signOut()}>logout</button>
     Home
-    <h1>Create concert</h1>
+    <CreateConcert />
   </div>
 }
