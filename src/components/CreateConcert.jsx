a2104700a31f32d454bdfa7371e72f0fc45e7ed8
Shafiya Adzhani <adz.arsym@gmail.com>
2020-10-12 08:59:38 +0800

    fix color on create concert

--- src/components/CreateConcert.jsx
+++ src/components/CreateConcert.jsx
@@ -15,12 +15,22 @@ const CreateConcert = () => {
   const [selectedOptions, setSelectedOptions] = useState(null);
 
   const onSubmit = (data) => {
-
     Promise.all(
-      selectedOptions.map(option => firebase.storage().ref().child("/merchandise/" + option.value)
-      .getDownloadURL()
-      .then(imageURL => ({imageURL, reference: store.collection("merchandise").doc(option.value), name: option.label, price: option.price})))) 
-      .then(merchandiseArray => firebase
+      selectedOptions.map((option) =>
+        firebase
+          .storage()
+          .ref()
+          .child("/merchandise/" + option.value)
+          .getDownloadURL()
+          .then((imageURL) => ({
+            imageURL,
+            reference: store.collection("merchandise").doc(option.value),
+            name: option.label,
+            price: option.price,
+          }))
+      )
+    ).then((merchandiseArray) =>
+      firebase
         .firestore()
         .collection("concerts")
         .add({
@@ -42,7 +52,8 @@ const CreateConcert = () => {
           start: data.start,
           end: data.end,
           imageUrl: data.imageUrl,
-        }));
+        })
+    );
   };
 
   let items = [];
@@ -50,7 +61,11 @@ const CreateConcert = () => {
   useEffect(() => {
     store
       .collection("merchandises")
-      .where("vendor", "==", store.collection("vendors").doc(auth.user && auth.user.uid))
+      .where(
+        "vendor",
+        "==",
+        store.collection("vendors").doc(auth.user && auth.user.uid)
+      )
       .get()
       .then(function (querySnapshot) {
         querySnapshot.docs.forEach((doc) => {
@@ -77,9 +92,17 @@ const CreateConcert = () => {
       <div className="flex flex-col px-4">
         <h1 className="text-center text-2xl">Create Concert</h1>
         <label className="text-white">Name</label>
-        <input name="name" ref={register} className="mb-2" />
+        <input
+          name="name"
+          ref={register}
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
+        />
         <label className="text-white">Description</label>
-        <input name="description" ref={register} className="mb-2" />
+        <input
+          name="description"
+          ref={register}
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
+        />
         <div className="flex flex-row mb-2">
           <div className="flex-1 flex flex-col">
             <label className="text-white">Concert Start</label>
@@ -94,6 +117,7 @@ const CreateConcert = () => {
                   showTimeSelect
                   dateFormat="dd MMMM yyyy HH:mm"
                   placeholderText="Select concert's start date"
+                  className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
                 />
               )}
             />
@@ -111,6 +135,7 @@ const CreateConcert = () => {
                   showTimeSelect
                   dateFormat="dd MMMM yyyy HH:mm"
                   placeholderText="Select concert's end date"
+                  className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
                 />
               )}
             />
@@ -130,6 +155,7 @@ const CreateConcert = () => {
                   showTimeSelect
                   dateFormat="dd MMMM yyyy HH:mm"
                   placeholderText="Select sale's start date"
+                  className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
                 />
               )}
             />
@@ -147,6 +173,7 @@ const CreateConcert = () => {
                   showTimeSelect
                   dateFormat="dd MMMM yyyy HH:mm"
                   placeholderText="Select sale's end date"
+                  className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
                 />
               )}
             />
@@ -158,7 +185,7 @@ const CreateConcert = () => {
           type="number"
           name="price"
           ref={register}
-          className="mb-2"
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
         ></input>
         <label className="text-white">Stock</label>
         <input
@@ -166,12 +193,20 @@ const CreateConcert = () => {
           type="number"
           name="stock"
           ref={register}
-          className="mb-2"
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
         ></input>
         <label className="text-white">Artist Name</label>
-        <input name="artistName" ref={register} className="mb-2"></input>
+        <input
+          name="artistName"
+          ref={register}
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
+        ></input>
         <label className="text-white">Image URL</label>
-        <input name="imageUrl" ref={register} className="mb-2"></input>
+        <input
+          name="imageUrl"
+          ref={register}
+          className="mb-2 bg-neutral-black-1 text-white border-b border-gray-200 p-2"
+        ></input>
         <label className="text-white">Merchandises</label>
         <Select
           defaultValue={selectedOptions}
@@ -180,7 +215,11 @@ const CreateConcert = () => {
           options={merchandise.length > 0 ? handleMerchandise(merchandise) : []}
         />
         <div className="mt-8">
-          <input type="submit" value="Add Concert"></input>
+          <input
+            type="submit"
+            value="Add Concert"
+            className="px-4 py-2 bg-primary-2 text-white "
+          ></input>
         </div>
       </div>
     </form>
